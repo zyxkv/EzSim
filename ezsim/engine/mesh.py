285a98a5453200006c9185b1979430a0eb4b3140
zyxkv <zeyu.robotics@gmail.com>
2025-08-07 14:45:49 +0800

    FOLLOW: remove fast_simplification submodule in ext

--- ezsim/engine/mesh.py
+++ ezsim/engine/mesh.py
@@ -1,6 +1,7 @@
 import os
 import pickle as pkl
 
+import fast_simplification
 import numpy as np
 import numpy.typing as npt
 import pyvista as pv
@@ -14,7 +15,7 @@ import ezsim.utils.mesh as mu
 import ezsim.utils.gltf as gltf_utils
 import ezsim.utils.usda as usda_utils
 import ezsim.utils.particle as pu
-from ezsim.ext import fast_simplification
+# from ezsim.ext import fast_simplification
 from ezsim.repr_base import RBC
 
 
@@ -93,15 +94,32 @@ class Mesh(RBC):
         """
         if self._mesh.vertices.shape[0] > 3 and self._mesh.faces.shape[0] > decimate_face_num:
             self._mesh.process(validate=True)
-            self._mesh = trimesh.Trimesh(
-                *fast_simplification.simplify(
-                    self._mesh.vertices,
-                    self._mesh.faces,
-                    target_count=decimate_face_num,
-                    agg=decimate_aggressiveness,
-                    lossless=(decimate_aggressiveness == 0),
+            # TODO: lossless option support is pending on fast_simplification package.
+            # NOTE: https://github.com/pyvista/fast-simplification/pull/71
+            if decimate_aggressiveness == 0:
+                gs.logger.debug("Lossless simplification is not supported yet. Not applying simplification.")
+                self._mesh = trimesh.Trimesh(
+                    vertices=self._mesh.vertices,
+                    faces=self._mesh.faces,
                 )
-            )
+            else:
+                self._mesh = trimesh.Trimesh(
+                    *fast_simplification.simplify(
+                        self._mesh.vertices,
+                        self._mesh.faces,
+                        target_count=decimate_face_num,
+                        agg=decimate_aggressiveness,
+                    )
+                )
+            # self._mesh = trimesh.Trimesh(
+            #     *fast_simplification.simplify(
+            #         self._mesh.vertices,
+            #         self._mesh.faces,
+            #         target_count=decimate_face_num,
+            #         agg=decimate_aggressiveness,
+            #         lossless=(decimate_aggressiveness == 0),
+            #     )
+            # )
 
             # need to run convexify again after decimation, because sometimes decimating a convex-mesh can make it non-convex...
             if convexify:
--- ezsim/utils/terrain.py
+++ ezsim/utils/terrain.py
@@ -2,11 +2,12 @@ import os
 import math
 import pickle
 
+import fast_simplification
 import numpy as np
 import trimesh
 
 import ezsim
-from ezsim.ext import fast_simplification
+# from ezsim.ext import fast_simplification
 from ezsim.ext.isaacgym import terrain_utils as isaacgym_terrain_utils
 from ezsim.options.morphs import Terrain
 
@@ -387,12 +388,21 @@ def convert_heightfield_to_watertight_trimesh(
     # This is the mesh used for non-sdf purposes.
     # It's losslessly simplified from the full mesh, to save memory cost for storing verts and faces.
 
-    v_simp, f_simp = fast_simplification.simplify(
-        sdf_mesh.vertices,
-        sdf_mesh.faces,
-        target_count=0,
-        lossless=True,
-    )
+    # v_simp, f_simp = fast_simplification.simplify(
+    #     sdf_mesh.vertices,
+    #     sdf_mesh.faces,
+    #     target_count=0,
+    #     lossless=True,
+    # )
+    
+    # TODO: lossless option support is pending on fast_simplification package.
+    # v_simp, f_simp = fast_simplification.simplify(
+    #     sdf_mesh.vertices,
+    #     sdf_mesh.faces,
+    #     target_count=0,
+    #     lossless=True,
+    # )
+    v_simp, f_simp = sdf_mesh.vertices, sdf_mesh.faces
 
     if uvs is not None:
         idx_map = np.empty(len(v_simp), dtype=np.int64)
